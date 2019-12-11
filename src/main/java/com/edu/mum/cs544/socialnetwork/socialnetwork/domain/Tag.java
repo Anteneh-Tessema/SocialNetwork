9e16ea302925b62fdb660f205927746bcb916021
Zelalem Zergaw <zola145103@gmail.com>
2019-12-11 17:57:02 -0600

    Last commit on domain models

--- src/main/java/com/edu/mum/cs544/socialnetwork/socialnetwork/domain/Tag.java
+++ src/main/java/com/edu/mum/cs544/socialnetwork/socialnetwork/domain/Tag.java
@@ -1,5 +1,9 @@
 package com.edu.mum.cs544.socialnetwork.socialnetwork.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.io.Serializable;
@@ -8,9 +12,11 @@ import java.util.UUID;
 
 
 @Entity
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Tag implements Serializable{
-	
-	
+
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -29,72 +35,5 @@ public class Tag implements Serializable{
 		this.deleted = Boolean.FALSE;
 		this.recordedDate = LocalDateTime.now();
 	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getPosts() {
-		return posts;
-	}
-
-	public void setPosts(String posts) {
-		this.posts = posts;
-	}
-
-	public boolean isDeleted() {
-		return deleted;
-	}
-
-	public void setDeleted(boolean deleted) {
-		this.deleted = deleted;
-	}
-
-	public LocalDateTime getRecordedDate() {
-		return recordedDate;
-	}
-
-	public void setRecordedDate(LocalDateTime recordedDate) {
-		this.recordedDate = recordedDate;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Tag other = (Tag) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
-	
 
 }
