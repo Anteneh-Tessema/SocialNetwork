634e0454ae795194cda51edf59969746e2323bb1
Zelalem Zergaw <zola145103@gmail.com>
2019-12-18 03:17:09 -0600

    adjusted commit

--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists persistent_logins (
-  username varchar_ignorecase(100) not null,
-  series varchar(64) primary key,
-  token varchar(64) not null,
-  last_used timestamp not null
-  );
-
-INSERT INTO Role VALUES (1, 'ADMIN');
-INSERT INTO Role VALUES (4, 'USER');
