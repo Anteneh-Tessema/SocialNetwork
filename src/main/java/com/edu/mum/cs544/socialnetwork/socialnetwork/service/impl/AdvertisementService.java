407b40d132a230ef43b8048e57c38a265763d1f4
Zelalem Zergaw <zola145103@gmail.com>
2019-12-19 13:48:52 -0600

    final commit for submission

--- src/main/java/com/edu/mum/cs544/socialnetwork/socialnetwork/service/impl/AdvertisementService.java
+++ src/main/java/com/edu/mum/cs544/socialnetwork/socialnetwork/service/impl/AdvertisementService.java
@@ -1,13 +1,17 @@
 package com.edu.mum.cs544.socialnetwork.socialnetwork.service.impl;
 
 import com.edu.mum.cs544.socialnetwork.socialnetwork.dao.Impl.AdvertisementRepository;
+import com.edu.mum.cs544.socialnetwork.socialnetwork.dao.UserDao;
 import com.edu.mum.cs544.socialnetwork.socialnetwork.domain.Advertisement;
 import com.edu.mum.cs544.socialnetwork.socialnetwork.service.IAdvertisementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -15,10 +19,12 @@ public class AdvertisementService implements IAdvertisementService {
 
     @Autowired
     private AdvertisementRepository advertisementRepository;
-
+    @Autowired
+    private UserDao userDao;
 
     @Override
     public List<Advertisement> getAllAdvertisementList() {
+
         return advertisementRepository.findAll();
     }
 
